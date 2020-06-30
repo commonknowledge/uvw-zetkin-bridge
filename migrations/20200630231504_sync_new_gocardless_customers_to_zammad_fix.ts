a420f5fa21148d2635ba9948741b5c17940c20cc
Jan Baykara <janbaykara@gmail.com>
2020-07-01 00:00:04 +0100

    Fix column drop reverse migration

--- migrations/20200630231504_sync_new_gocardless_customers_to_zammad_fix.ts
+++ migrations/20200630231504_sync_new_gocardless_customers_to_zammad_fix.ts
@@ -10,7 +10,7 @@ export async function up(knex: Knex): Promise<any> {
 
 export async function down(knex: Knex): Promise<any> {
   return knex.schema.table('gocardless_customers', function(t) {
-    t.boolean('synced_to_zammad')
+    t.dropColumn('synced_to_zammad')
   });
 }
 
