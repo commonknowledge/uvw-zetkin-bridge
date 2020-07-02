4c65b600161c12e5ca1a50699c2218ecf91082b1
Jan Baykara <janbaykara@gmail.com>
2020-07-02 10:03:46 +0100

    Sync more data from GoCardless to Zammad

--- src/gocardless/scripts/cache-customers.ts
+++ src/gocardless/scripts/cache-customers.ts
@@ -1,8 +1,8 @@
-import { getGoCardlessPaginatedList } from '../gocardless';
+import { getGoCardlessPaginatedList, getRelevantZetkinDataFromGoCardlessCustomer } from '../gocardless';
 import db from '../../db';
 import * as GoCardless from 'gocardless-nodejs';
 import { chunk } from 'lodash';
-import { upsertZammadUser } from '../../zammad/zammad';
+import { upsertZammadUser, ZammadUser } from '../../zammad/zammad';
 import Phone from 'awesome-phonenumber';
 
 const saveAllCustomersToDatabase = async () => {
@@ -59,12 +59,25 @@ const syncCustomersToZammad = async () => {
     for (const { id, created_at, ...person } of people) {
       try {
         console.log(unsynced.length, synced.length)
-        const newPerson = {
+        const paymentData = await getRelevantZetkinDataFromGoCardlessCustomer(id)
+        const newPerson: Partial<ZammadUser> = {
+          ...person,
+          ...paymentData,
+          address: [
+            person.address_line1,
+            person.address_line2,
+            person.address_line3,
+            person.city,
+            person.region
+          ].filter(Boolean).join(",\n"),
+          zip: person.postal_code,
           login: String(
             person.email || person.phone_number || `${person.given_name} ${person.family_name}`
           ).replace(/\s/mig, ''),
-          ...person,
-          mobile: person.phone_number ? new Phone(String(person.phone_number), 'GB').getNumber('e164') : undefined
+          firstname: person.given_name,
+          lastname: person.family_name,
+          mobile: person.phone_number ? new Phone(String(person.phone_number), 'GB').getNumber('e164') : undefined,
+          phone: person.phone_number ? new Phone(String(person.phone_number), 'GB').getNumber('e164') : undefined
         }
         // Create/update Zammad
         const zammadUser = await upsertZammadUser(newPerson)
